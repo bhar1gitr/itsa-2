5a2d012a385f6c2d3dc78067bba25e90567d73ba
Bharat Sharma <21104023bharatsharma@gmail.com>
2023-09-21 22:57:55 +0530

    Update ProgressLine.js

--- client/src/components/Home/ProgressLine.js
+++ client/src/components/Home/ProgressLine.js
@@ -1,41 +1,104 @@
-import { Chrono } from "react-chrono";
-import {Link} from "react-router-dom"
-
-const customContent = [
-  <Link style={{textDecoration:"none"}} to="/events">
-  <div>
-    <h3 style={{color:"white"}}>Learn Course Online</h3> 
-  </div>
-    </Link>,
-  <div>
-    <h3 style={{color:"white"}}>Faculty Profile</h3>
-  </div>,
-   <div>
-   <h3 style={{color:"white"}}>Student Organization</h3>
- </div>,
-  <div>
-  <h3 style={{color:"white"}}>Department Achivements</h3>
-</div>,
-];
+import {Link} from "react-router-dom";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import {FiveMpOutlined,WorkspacesSharp} from "@mui/icons-material"
 
 const VerticalAlternatingTimeline = () => {
   return (
     <div>
     <h1 style={{textAlign:'center',marginTop:"20px",marginBottom:"20px"}}>Time-Line</h1>
-
-    <Chrono
-      mode="VERTICAL_ALTERNATING"
-      cardHeight={50} 
-      mediaHeight={500}
-      cardWidth={400}
-      slideItemDuration={1000} slideShow
-      theme={{
-        cardBgColor:"#27005D",
-        secondary: "white",
-      }}
-    >
-      {customContent}
-    </Chrono>
+    <VerticalTimeline animate='true' lineColor='#1976d2'>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+    date="2011 - present"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<WorkspacesSharp />}
+  >
+    <h3 className="vertical-timeline-element-title">Creative Director</h3>
+    <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+    <p>
+      Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="2010 - 2011"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<WorkspacesSharp />}
+  >
+    <h3 className="vertical-timeline-element-title">Art Director</h3>
+    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+    <p>
+      Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="2008 - 2010"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<WorkspacesSharp />}
+  >
+    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+    <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+    <p>
+      User Experience, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="2006 - 2008"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<WorkspacesSharp />}
+  >
+    <h3 className="vertical-timeline-element-title">Web Designer</h3>
+    <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+    <p>
+      User Experience, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="April 2013"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    icon={<FiveMpOutlined />}
+  >
+    <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+    <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+    <p>
+      Strategy, Social Media
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="November 2012"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    icon={<FiveMpOutlined />}
+  >
+    <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+    <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+    <p>
+      Creative Direction, User Experience, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--education"
+    date="2002 - 2006"
+    iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+    icon={<FiveMpOutlined />}
+  >
+    <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+    <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+    <p>
+      Creative Direction, Visual Design
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+    icon={<FiveMpOutlined />}
+  />
+</VerticalTimeline>
     </div>
   );
 };
